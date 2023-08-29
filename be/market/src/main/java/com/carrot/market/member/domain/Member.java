bca67eb4432aa9b8af726158ecc510710f52ced4
JJONSOO <pjs970616@naver.com>
2023-08-30 00:20:56 +0900

    feat: refreshToken 저장, Builder 적용

--- be/market/src/main/java/com/carrot/market/member/domain/Member.java
+++ be/market/src/main/java/com/carrot/market/member/domain/Member.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -45,4 +46,22 @@ public class Member extends BaseEntity {
 
 	@LastModifiedDate
 	private LocalDateTime modifiedAt;
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	@Builder
+	public Member(String nickname, String imageUrl,
+		List<MemberLocation> memberLocations, List<WishList> wishLists,
+		List<Sales> sales, String refreshToken, String socialId, LocalDateTime modifiedAt) {
+		this.nickname = nickname;
+		this.imageUrl = imageUrl;
+		this.memberLocations = memberLocations;
+		this.wishLists = wishLists;
+		this.sales = sales;
+		this.refreshToken = refreshToken;
+		this.socialId = socialId;
+		this.modifiedAt = modifiedAt;
+	}
 }
