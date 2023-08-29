09c1e45e65ee1f2b6c870181db78041a5dcf27a5
JJONSOO <pjs970616@naver.com>
2023-08-30 00:29:03 +0900

    feat: Member 회원가입

---
+++ be/market/src/main/java/com/carrot/market/member/application/MemberService.java
@@ -0,0 +1,61 @@
+package com.carrot.market.member.application;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.carrot.market.global.exception.ApiException;
+import com.carrot.market.global.exception.domain.LocationException;
+import com.carrot.market.jwt.application.JwtProvider;
+import com.carrot.market.jwt.domain.Jwt;
+import com.carrot.market.location.infrastructure.LocationRepository;
+import com.carrot.market.member.application.dto.request.SignupServiceRequest;
+import com.carrot.market.member.application.dto.response.LoginMemberResponse;
+import com.carrot.market.member.domain.Member;
+import com.carrot.market.member.domain.MemberLocation;
+import com.carrot.market.member.infrastructure.MemberLocationRepository;
+import com.carrot.market.member.infrastructure.MemberRepository;
+import com.carrot.market.oauth.application.dto.response.LoginResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+	private final String MEMBER_ID = "memberId";
+	private final MemberRepository memberRepository;
+	private final LocationRepository locationRepository;
+	private final MemberLocationRepository memberLocationRepository;
+	private final JwtProvider jwtProvider;
+
+	@Transactional
+	public LoginResponse signup(SignupServiceRequest signupServiceRequest) {
+		Member member = signupServiceRequest.toMember();
+
+		Member savedMember = memberRepository.save(member);
+
+		Jwt jwt = jwtProvider.createJwt(Map.of(MEMBER_ID, savedMember.getId()));
+		savedMember.setRefreshToken(jwt.getRefreshToken());
+		LoginMemberResponse loginMemberResponse = new LoginMemberResponse(savedMember.getId(),
+			savedMember.getNickname());
+
+		saveMemberLocation(true, member, signupServiceRequest.mainLocationId());
+
+		saveMemberLocation(false, member, signupServiceRequest.subLocationId());
+
+		return LoginResponse.success(jwt, loginMemberResponse);
+	}
+
+	private MemberLocation saveMemberLocation(boolean isMain, Member member, Long signupServiceRequest) {
+		return memberLocationRepository.save(
+			MemberLocation.builder()
+				.isMain(isMain)
+				.member(member)
+				.location(locationRepository.findById(signupServiceRequest)
+					.orElseThrow(() -> new ApiException(LocationException.NOT_FOUND_ID)))
+				.build()
+		);
+	}
+}
---
+++ be/market/src/main/java/com/carrot/market/member/presentation/MemberController.java
@@ -0,0 +1,32 @@
+package com.carrot.market.member.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carrot.market.member.application.MemberService;
+import com.carrot.market.member.presentation.dto.request.SignupRequest;
+import com.carrot.market.oauth.application.dto.response.LoginResponse;
+import com.carrot.market.oauth.domain.OauthMember;
+import com.carrot.market.oauth.presentation.annotation.OauthLogin;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/signup")
+	ResponseEntity<LoginResponse> signup(
+		@RequestBody SignupRequest signupRequest,
+		@OauthLogin OauthMember oauthUser
+	) {
+		LoginResponse response = memberService.signup(
+			signupRequest.toSignupServiceRequest(oauthUser.getImageUrl(), oauthUser.getSocialId()));
+
+		return ResponseEntity.ok(response);
+	}
+}
