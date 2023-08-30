2ec76afd3feffe422e0980b99c8ddb76536f4ad8
JJONSOO <pjs970616@naver.com>
2023-08-30 12:29:18 +0900

    fix: api uri 수정

--- be/market/src/main/java/com/carrot/market/member/presentation/MemberController.java
+++ be/market/src/main/java/com/carrot/market/member/presentation/MemberController.java
@@ -3,6 +3,7 @@ package com.carrot.market.member.presentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carrot.market.member.application.MemberService;
@@ -13,6 +14,7 @@ import com.carrot.market.oauth.presentation.annotation.OauthLogin;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
