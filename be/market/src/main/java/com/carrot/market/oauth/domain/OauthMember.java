eaad3e8aba7037c67e7fb5b3362820b334685c93
JJONSOO <pjs970616@naver.com>
2023-08-30 12:36:10 +0900

    fix: lombok 으로 대체한다.

--- be/market/src/main/java/com/carrot/market/oauth/domain/OauthMember.java
+++ be/market/src/main/java/com/carrot/market/oauth/domain/OauthMember.java
@@ -2,20 +2,17 @@ package com.carrot.market.oauth.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OauthMember {
 	@JsonProperty("socialId")
 	private String socialId;
 	@JsonProperty("imageUrl")
 	private String imageUrl;
 
-	public OauthMember() {
-	}
-
-	public OauthMember(String socialId, String imageUrl) {
-		this.socialId = socialId;
-		this.imageUrl = imageUrl;
-	}
 }
