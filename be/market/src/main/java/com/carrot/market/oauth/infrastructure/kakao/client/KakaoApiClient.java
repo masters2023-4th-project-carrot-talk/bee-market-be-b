38894f11145d1e0c8a5b815ed9bb1bcfce279da1
JJONSOO <pjs970616@naver.com>
2023-08-30 12:15:18 +0900

    fix: RequestParameter에 name추가

--- be/market/src/main/java/com/carrot/market/oauth/infrastructure/kakao/client/KakaoApiClient.java
+++ be/market/src/main/java/com/carrot/market/oauth/infrastructure/kakao/client/KakaoApiClient.java
@@ -15,7 +15,7 @@ import com.carrot.market.oauth.infrastructure.kakao.dto.KakaoToken;
 public interface KakaoApiClient {
 
 	@PostExchange(url = "https://kauth.kakao.com/oauth/token", contentType = APPLICATION_FORM_URLENCODED_VALUE)
-	KakaoToken fetchToken(@RequestParam MultiValueMap<String, String> params);
+	KakaoToken fetchToken(@RequestParam(name = "params") MultiValueMap<String, String> params);
 
 	@GetExchange("https://kapi.kakao.com/v2/user/me")
 	KakaoMemberResponse fetchMember(@RequestHeader(name = AUTHORIZATION) String bearerToken);
