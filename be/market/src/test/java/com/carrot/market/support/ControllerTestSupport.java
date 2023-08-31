7281ff44497c325127ee1f6a2d0fc7d44740b6c7
JJONSOO <pjs970616@naver.com>
2023-08-31 14:58:08 +0900

    feat : Product Controller Test

--- be/market/src/test/java/com/carrot/market/support/ControllerTestSupport.java
+++ be/market/src/test/java/com/carrot/market/support/ControllerTestSupport.java
@@ -2,11 +2,15 @@ package com.carrot.market.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.carrot.market.product.application.ProductService;
+import com.carrot.market.product.presentation.ProductController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = {
+	ProductController.class
 })
 public abstract class ControllerTestSupport {
 	@Autowired
@@ -14,4 +18,8 @@ public abstract class ControllerTestSupport {
 
 	@Autowired
 	protected ObjectMapper objectMapper;
+
+	@MockBean
+	protected ProductService productService;
+
 }
