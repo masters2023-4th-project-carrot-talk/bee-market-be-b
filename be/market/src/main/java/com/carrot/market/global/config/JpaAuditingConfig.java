aefb4f7701c1891c02017064fbe3beed52f41129
JJONSOO <pjs970616@naver.com>
2023-08-31 22:05:26 +0900

    feat: JpaQueryFactory 추가

--- be/market/src/main/java/com/carrot/market/global/config/JpaAuditingConfig.java
+++ be/market/src/main/java/com/carrot/market/global/config/JpaAuditingConfig.java
@@ -1,9 +1,22 @@
 package com.carrot.market.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @EnableJpaAuditing
 @Configuration
 public class JpaAuditingConfig {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory queryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
 }
