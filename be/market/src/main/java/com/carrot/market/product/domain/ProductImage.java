86ed5d70b97dde1c059deb8b25bc197d87fd0b78
code-tree <dltjdqls1200@naver.com>
2023-09-01 11:27:04 +0900

    chore: 공백 제거, generatedValue 추가

--- be/market/src/main/java/com/carrot/market/product/domain/ProductImage.java
+++ be/market/src/main/java/com/carrot/market/product/domain/ProductImage.java
@@ -6,6 +6,7 @@ import com.carrot.market.image.domain.Image;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -19,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class ProductImage extends BaseEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
