4af24b9e30eeb84fc72c610f9b21adcd2825437d
JJONSOO <pjs970616@naver.com>
2023-09-05 15:09:27 +0900

    feat: ProductRepository 조회수 조회 및 업데이트 query 추가

--- be/market/src/main/java/com/carrot/market/product/infrastructure/ProductRepository.java
+++ be/market/src/main/java/com/carrot/market/product/infrastructure/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.carrot.market.product.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carrot.market.product.application.dto.response.ProductSellerDetaillDto;
@@ -21,4 +23,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	)
 	ProductSellerDetaillDto findProductDetailbyId(Long productId);
 
+	@Query("select p.viewCount from Product p where p.id = :id")
+	Long findViewCount(@Param("id") Long id);
+
+	@Modifying(clearAutomatically = true)
+	@Query("update Product p set p.viewCount = :viewCnt where p.id = :id")
+	void applyViewCntToRDB(@Param("id") Long id, @Param("viewCnt") Long viewCnt);
 }
