2b50d736fa2bedf05ae2d36155d43755ab4a16b8
JJONSOO <pjs970616@naver.com>
2023-09-05 15:06:59 +0900

    fix: test시 Scheduling 실행을 관리하도록 수정

--- be/market/src/test/java/com/carrot/market/support/CacheTestSupport.java
+++ be/market/src/test/java/com/carrot/market/support/CacheTestSupport.java
@@ -3,11 +3,9 @@ package com.carrot.market.support;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.transaction.annotation.Transactional;
 
-@TestPropertySource(properties = "app.scheduling.enable=false")
-@Transactional
+@TestPropertySource(properties = "app.scheduling.enable=true")
 @ActiveProfiles("test")
 @SpringBootTest
-public abstract class IntegrationTestSupport {
+public abstract class CacheTestSupport {
 }
