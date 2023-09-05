e6a1454cb242271d5351d6adf3ef5b24e3534345
JJONSOO <pjs970616@naver.com>
2023-09-05 18:06:51 +0900

    fix : 사용하지 않는 주석, log 삭제

--- be/market/src/main/java/com/carrot/market/global/config/RedisConfig.java
+++ be/market/src/main/java/com/carrot/market/global/config/RedisConfig.java
@@ -8,9 +8,6 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Configuration
 public class RedisConfig {
 
@@ -23,25 +20,16 @@ public class RedisConfig {
 	// 커넥션 설정
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		log.info(host);
 		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 		redisStandaloneConfiguration.setHostName(host);
 		redisStandaloneConfiguration.setPort(port);
-		//		redisStandaloneConfiguration.setPassword(password);
+
 		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
 	public RedisTemplate<?, ?> redisTemplate() {
-
-		// redisTemplate 를 받아와서 set, get, delete 를 사용
 		RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-		/*
-		 * setKeySerializer, setValueSerializer 설정
-		 * redis-cli 을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-		 */
-		//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-		//        redisTemplate.setValueSerializer(new StringRedisSerializer());
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 
 		return redisTemplate;
