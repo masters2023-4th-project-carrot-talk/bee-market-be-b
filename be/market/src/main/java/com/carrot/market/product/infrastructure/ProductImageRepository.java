aba5d7084e03595a92a5a2f6efed87b1f3b7a57b
JJONSOO <pjs970616@naver.com>
2023-09-06 11:13:44 +0900

    fix : sql에 productId 추가

--- be/market/src/main/java/com/carrot/market/product/infrastructure/ProductImageRepository.java
+++ be/market/src/main/java/com/carrot/market/product/infrastructure/ProductImageRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carrot.market.product.domain.ProductImage;
@@ -11,6 +12,6 @@ import com.carrot.market.product.domain.ProductImage;
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
-	@Query("select pi.image.imageUrl from ProductImage as pi join pi.image")
-	List<String> findImageUrlsbyPrdcutId(Long productId);
+	@Query("select pi.image.imageUrl from ProductImage as pi join pi.image where pi.product.id = :productId")
+	List<String> findImageUrlsbyPrdcutId(@Param("productId") Long productId);
 }
