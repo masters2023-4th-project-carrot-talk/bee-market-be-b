fcdc210d115017a9f13a790112947e1ef27a442a
JJONSOO <pjs970616@naver.com>
2023-09-06 16:25:10 +0900

    fix: login시 authcode를 바디로 받는다.

--- be/market/src/main/java/com/carrot/market/oauth/presentation/OauthController.java
+++ be/market/src/main/java/com/carrot/market/oauth/presentation/OauthController.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carrot.market.oauth.application.OauthService;
 import com.carrot.market.oauth.application.dto.response.LoginResponse;
+import com.carrot.market.oauth.presentation.dto.request.LoginRequest;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,11 +32,11 @@ public class OauthController {
 		return ResponseEntity.ok().build();
 	}
 
-	@GetMapping("/api/users/login")
+	@PostMapping("/api/users/login")
 	ResponseEntity<LoginResponse> login(
-		@RequestParam(name = "code") String code
+		@RequestBody LoginRequest loginRequest
 	) {
-		LoginResponse login = oauthService.login(code);
+		LoginResponse login = oauthService.login(loginRequest.code());
 		return ResponseEntity.ok(login);
 	}
 
