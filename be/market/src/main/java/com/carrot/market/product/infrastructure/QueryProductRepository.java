c70f853352f034dec19a7da8b77d37b840e4f5d0
JJONSOO <pjs970616@naver.com>
2023-09-06 16:26:18 +0900

    feat: 관심상품, 판매목록 Repository test 구현

--- be/market/src/main/java/com/carrot/market/product/infrastructure/QueryProductRepository.java
+++ be/market/src/main/java/com/carrot/market/product/infrastructure/QueryProductRepository.java
@@ -16,8 +16,10 @@ import org.springframework.stereotype.Repository;
 
 import com.carrot.market.member.domain.QMember;
 import com.carrot.market.product.domain.Product;
-import com.carrot.market.product.infrastructure.dto.MainPageSliceDto;
-import com.carrot.market.product.infrastructure.dto.QMainPageSliceDto;
+import com.carrot.market.product.domain.SellingStatus;
+import com.carrot.market.product.infrastructure.dto.request.DetailPageSliceRequestDto;
+import com.carrot.market.product.infrastructure.dto.response.DetailPageSliceResponseDto;
+import com.carrot.market.product.infrastructure.dto.response.QDetailPageSliceResponseDto;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.JPQLQueryFactory;
@@ -30,10 +32,10 @@ public class QueryProductRepository {
 	private static final Long BASIC_LOCATION_ID = 1L;
 	private final JPQLQueryFactory queryFactory;
 
-	public Slice<MainPageSliceDto> findByLocationIdAndCategoryId(Long locationId, Long categoryId,
-		Long productId, int pageSize) {
-		List<MainPageSliceDto> fetch = queryFactory
-			.select(new QMainPageSliceDto(
+	public Slice<DetailPageSliceResponseDto> findByDetailPageSliceRequestDto(
+		DetailPageSliceRequestDto detailPageSliceRequestDto) {
+		List<DetailPageSliceResponseDto> fetch = queryFactory
+			.select(new QDetailPageSliceResponseDto(
 					product.id,
 					product.seller.id,
 					product.productDetails.name,
@@ -48,9 +50,9 @@ public class QueryProductRepository {
 			)
 			.from(product)
 			.where(
-				lessThanItemId(productId),
-				locationIdEq(locationId),
-				categoryEq(categoryId)
+				lessThanItemId(detailPageSliceRequestDto.nextProductId()),
+				locationIdEq(detailPageSliceRequestDto.locationId()),
+				categoryEq(detailPageSliceRequestDto.categoryId())
 			)
 			.orderBy(product.id.desc())
 			.leftJoin(product.location, location)
@@ -68,17 +70,77 @@ public class QueryProductRepository {
 			.groupBy(product.createdAt)
 			.groupBy(product.productDetails.price)
 			.groupBy(product.status.stringValue())
-			.limit(pageSize + 1)
+			.limit(detailPageSliceRequestDto.pageSize() + 1)
 			.fetch();
 
 		JPQLQuery<Product> count = queryFactory
 			.selectFrom(product)
 			.where(
-				locationIdEq(locationId),
-				categoryEq(categoryId)
+				locationIdEq(detailPageSliceRequestDto.locationId()),
+				categoryEq(detailPageSliceRequestDto.categoryId())
 			);
 
-		return PageableExecutionUtils.getPage(fetch, Pageable.ofSize(pageSize), () -> count.fetchCount());
+		return PageableExecutionUtils.getPage(fetch, Pageable.ofSize(detailPageSliceRequestDto.pageSize()),
+			() -> count.fetchCount());
+
+	}
+
+	public Slice<DetailPageSliceResponseDto> findByMyDetailPageSliceRequestDto(
+		DetailPageSliceRequestDto detailPageSliceRequestDto) {
+		List<DetailPageSliceResponseDto> fetch = queryFactory
+			.select(new QDetailPageSliceResponseDto(
+					product.id,
+					product.seller.id,
+					product.productDetails.name,
+					location.name,
+					image.imageUrl,
+					product.createdAt,
+					product.productDetails.price,
+					product.status.stringValue(),
+					wishList.countDistinct(),
+					chatroom.countDistinct()
+				)
+			)
+			.from(product)
+			.where(
+				lessThanItemId(detailPageSliceRequestDto.nextProductId()),
+				statusEq(detailPageSliceRequestDto.status()),
+				categoryEq(detailPageSliceRequestDto.categoryId()),
+				sellerdEq(detailPageSliceRequestDto.sellerId()),
+				wishListMemberEq(detailPageSliceRequestDto.wishMemberId())
+
+			)
+			.orderBy(product.id.desc())
+			.leftJoin(product.location, location)
+			.leftJoin(product.seller, new QMember("m1"))
+			.leftJoin(product.productImages, productImage)
+			.on(productImage.isMain.eq(true))
+			.leftJoin(productImage.image, image)
+			.leftJoin(product.wishLists, wishList)
+			.leftJoin(product.chatrooms, chatroom)
+			.groupBy(product.id)
+			.groupBy(product.seller.id)
+			.groupBy(product.productDetails.name)
+			.groupBy(location.name)
+			.groupBy(image.imageUrl)
+			.groupBy(product.createdAt)
+			.groupBy(product.productDetails.price)
+			.groupBy(product.status.stringValue())
+			.limit(detailPageSliceRequestDto.pageSize() + 1)
+			.fetch();
+
+		JPQLQuery<Product> count = queryFactory
+			.selectFrom(product)
+			.where(
+				categoryEq(detailPageSliceRequestDto.categoryId()),
+				statusEq(detailPageSliceRequestDto.status()),
+				sellerdEq(detailPageSliceRequestDto.sellerId()),
+				wishListMemberEq(detailPageSliceRequestDto.wishMemberId())
+			)
+			.leftJoin(product.wishLists, wishList);
+
+		return PageableExecutionUtils.getPage(fetch, Pageable.ofSize(detailPageSliceRequestDto.pageSize()),
+			() -> count.fetchCount());
 	}
 
 	private BooleanExpression lessThanItemId(Long itemId) {
@@ -89,11 +151,33 @@ public class QueryProductRepository {
 		return product.id.loe(itemId);
 	}
 
+	private BooleanExpression statusEq(String status) {
+		if (status == null) {
+			return null;
+		}
+		return product.status.eq(SellingStatus.valueOf(status));
+
+	}
+
 	private BooleanExpression locationIdEq(Long locationId) {
 		if (locationId == null) {
 			return product.location.id.eq(BASIC_LOCATION_ID);
 		}
 		return product.location.id.eq(locationId);
+	}
+
+	private BooleanExpression sellerdEq(Long memberId) {
+		if (memberId == null) {
+			return null;
+		}
+		return product.seller.id.eq(memberId);
+	}
+
+	private BooleanExpression wishListMemberEq(Long memberId) {
+		if (memberId == null) {
+			return null;
+		}
+		return wishList.member.id.eq(memberId);
 	}
 
 	private BooleanExpression categoryEq(Long categoryId) {
