2028cbcac3e115e3391ee621e061bf79b6827b24
JJONSOO <pjs970616@naver.com>
2023-09-06 16:16:43 +0900

    fix: resolver 직접 추가

--- be/market/src/main/java/com/carrot/market/member/resolver/MemberArgumentResolver.java
+++ be/market/src/main/java/com/carrot/market/member/resolver/MemberArgumentResolver.java
@@ -1,7 +1,6 @@
 package com.carrot.market.member.resolver;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -11,14 +10,13 @@ import com.carrot.market.member.presentation.annotation.Login;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@Component
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 	private final String MEMBER_ID = "memberId";
 
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
-		boolean hasUserType = Long.class.isAssignableFrom(parameter.getParameterType());
+		boolean hasUserType = MemberId.class.isAssignableFrom(parameter.getParameterType());
 
 		return hasLoginAnnotation && hasUserType;
 	}
@@ -27,6 +25,8 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
 		HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
-		return request.getAttribute(MEMBER_ID);
+		return new MemberId((Integer)request.getAttribute(MEMBER_ID));
+
 	}
+
 }
