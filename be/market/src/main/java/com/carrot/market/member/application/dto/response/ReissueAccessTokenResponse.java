2a596015ff8e098a279b4eb8ee099a5b3eeb3cf4
JJONSOO <pjs970616@naver.com>
2023-09-06 16:18:43 +0900

    feat: reissuetoken 발행

---
+++ be/market/src/main/java/com/carrot/market/member/application/dto/response/ReissueAccessTokenResponse.java
@@ -0,0 +1,6 @@
+package com.carrot.market.member.application.dto.response;
+
+public record ReissueAccessTokenResponse(
+	String accessToken
+) {
+}
--- be/market/src/main/java/com/carrot/market/member/presentation/MemberController.java
+++ be/market/src/main/java/com/carrot/market/member/presentation/MemberController.java
@@ -16,8 +16,13 @@ import com.carrot.market.global.presentation.ApiResponse;
 import com.carrot.market.member.application.MemberService;
 import com.carrot.market.member.application.dto.response.MainLocationResponse;
 import com.carrot.market.member.application.dto.response.MemberLocationResponse;
+import com.carrot.market.member.application.dto.response.ReissueAccessTokenResponse;
+import com.carrot.market.member.presentation.annotation.Login;
+import com.carrot.market.member.presentation.dto.request.LogoutRequest;
+import com.carrot.market.member.presentation.dto.request.ReissueAccessTokenRequest;
 import com.carrot.market.member.presentation.dto.request.SignupRequest;
 import com.carrot.market.member.presentation.dto.request.UpdateLocationRequest;
+import com.carrot.market.member.resolver.MemberId;
 import com.carrot.market.oauth.application.dto.response.LoginResponse;
 import com.carrot.market.oauth.domain.OauthMember;
 import com.carrot.market.oauth.presentation.annotation.OauthLogin;
@@ -48,6 +53,24 @@ public class MemberController {
 			signupRequest.toSignupServiceRequest(oauthUser.getImageUrl(), oauthUser.getSocialId()));
 
 		return ApiResponse.success(response);
+	}
+
+	@PostMapping("/reissue-access-token")
+	ApiResponse<ReissueAccessTokenResponse> reissueAccessToken(
+		@RequestBody ReissueAccessTokenRequest reissueAccessTokenRequest
+	) {
+		ReissueAccessTokenResponse reissueAccessTokenResponse = memberService.reissueAccessToken(
+			reissueAccessTokenRequest);
+		return ApiResponse.success(reissueAccessTokenResponse);
+	}
+
+	@PostMapping("logout")
+	public ApiResponse<Void> logout(
+		@RequestBody LogoutRequest logoutRequest,
+		@Login MemberId memberId
+	) {
+		memberService.logout(memberId.getMemberID(), logoutRequest.refreshToken());
+		return ApiResponse.successNoBody();
 	}
 
 	@GetMapping("/locations")
