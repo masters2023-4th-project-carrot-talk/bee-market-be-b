89c758e4e7ab740c92d877d6cf5ed788b853c1bb
code-tree <dltjdqls1200@naver.com>
2023-09-07 17:35:32 +0900

    feat: 이미지 전송 개별, 총 크기 초과시 예외 처리

--- be/market/src/main/java/com/carrot/market/global/exception/domain/ImageException.java
+++ be/market/src/main/java/com/carrot/market/global/exception/domain/ImageException.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ImageException implements CustomException {
 
-	IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다.");
+	IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다."),
+	MAX_UPLOAD_SIZE_EXCEEDED(HttpStatus.PAYLOAD_TOO_LARGE, "이미지 업로드 크기가 초과됐습니다."),
+	IMAGE_RESIZE_FAILED(HttpStatus.BAD_REQUEST, "이미지 사이즈 조정에 실패했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
