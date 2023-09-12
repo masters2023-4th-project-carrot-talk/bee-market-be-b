bf3ebde878a0bbc0a053b05b65b6889de9225356
JJONSOO <pjs970616@naver.com>
2023-09-12 17:42:15 +0900

    feat: 외부 메세지 브로커로 카프카를 도입

---
+++ be/market/src/main/java/com/carrot/market/global/config/interceptor/StompHandler.java
@@ -0,0 +1,71 @@
+package com.carrot.market.global.config.interceptor;
+
+import static com.carrot.market.global.filter.JwtAuthorizationFilter.*;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+import com.carrot.market.global.exception.domain.JwtException;
+import com.carrot.market.jwt.application.JwtProvider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+	private final JwtProvider jwtProvider;
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+		// StompCommand에 따라서 로직을 분기해서 처리하는 메서드를 호출한다.
+		log.info("StompAccessor = {}", accessor);
+		handleMessage(accessor.getCommand(), accessor);
+		return message;
+	}
+
+	private void handleMessage(StompCommand stompCommand, StompHeaderAccessor accessor) {
+		switch (stompCommand) {
+
+			case CONNECT:
+				log.info("CONNECT !!");
+				getMemberId(accessor);
+				break;
+			case SUBSCRIBE:
+				log.info("SUBSCRIBE !!");
+			case SEND:
+				break;
+		}
+	}
+
+	private String getAccessToken(StompHeaderAccessor accessor) {
+		return accessor.getFirstNativeHeader("Authorization");
+	}
+
+	private Long getMemberId(StompHeaderAccessor accessor) {
+		try {
+			String token = getAccessToken(accessor);
+			Claims claims = jwtProvider.getClaims(token);
+			return Long.valueOf((Integer)claims.get(MEMBER_ID));
+		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException |
+			IllegalArgumentException ex) {
+			throw new IllegalStateException(JwtException.from(ex).getMessage());
+		}
+	}
+
+}
---
+++ be/market/src/main/java/com/carrot/market/global/util/KafkaConstant.java
@@ -0,0 +1,7 @@
+package com.carrot.market.global.util;
+
+public abstract class KafkaConstant {
+	public static final String KAFKA_TOPIC = "bee-chat";
+	public static final String GROUP_ID = "foo";
+	public static final String KAFKA_BROKER = "localhost:9092";
+}
