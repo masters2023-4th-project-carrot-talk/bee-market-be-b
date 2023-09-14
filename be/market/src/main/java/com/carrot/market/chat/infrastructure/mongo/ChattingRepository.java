b8dd66cd8063ad197c7d72f2835307ea44f01f26
JJONSOO <pjs970616@naver.com>
2023-09-14 20:03:25 +0900

    fix: Repository Annotation 추가

--- be/market/src/main/java/com/carrot/market/chat/infrastructure/mongo/ChattingRepository.java
+++ be/market/src/main/java/com/carrot/market/chat/infrastructure/mongo/ChattingRepository.java
@@ -1,9 +1,11 @@
 package com.carrot.market.chat.infrastructure.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.carrot.market.chat.domain.Chatting;
 
+@Repository
 public interface ChattingRepository extends MongoRepository<Chatting, String> {
 
 }
