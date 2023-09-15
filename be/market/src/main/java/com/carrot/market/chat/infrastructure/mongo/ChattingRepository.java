c47043ccfc5d43f5ccc059983d4bef98b518d538
JJONSOO <75569293+JJONSOO@users.noreply.github.com>
2023-09-15 13:17:43 +0900

    채팅목록 가져오기 리팩토링 (#36)
    * feat: 안 읽은 채팅 수, 마지막 채팅 내역
    
    * feat: 특정 memberId의 채팅창 목록 가져오기
    
    * feat: 특정 memberId의 채팅창 목록, 채팅 정보 가져오기
    
    * fix: 안쓰는 memberRepository 삭제
    
    * feat: Index 추가, readChatting 추가
    
    * fix: Repository Annotation 추가
    
    * feat: 채팅방 정보 가져오는 Response
    
    * feat: 채팅방 정보 , 안 읽은 채팅 수, 마지막 채팅 메세지 가져오는 Response
    
    * feat: 채팅방 정보 , 안 읽은 채팅 수, 마지막 채팅 메세지 가져오는 테스트 구현
    
    * feat: 채팅방 시간 가져오기
    
    * feat: 채팅 목록 가져오기
    
    * feat: 채팅 목록 가져오기
    
    * fix: 채팅목록 가져오기 서비스에서 말고 repository에서 query문 작성하도록 수정

--- be/market/src/main/java/com/carrot/market/chat/infrastructure/mongo/ChattingRepository.java
+++ be/market/src/main/java/com/carrot/market/chat/infrastructure/mongo/ChattingRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.carrot.market.chat.domain.Chatting;
+import com.carrot.market.chatroom.application.dto.response.ChatroomInfo;
 
 @Repository
 public interface ChattingRepository extends MongoRepository<Chatting, String> {
@@ -18,11 +19,14 @@ public interface ChattingRepository extends MongoRepository<Chatting, String> {
 	})
 	List<Chatting> findByChatRoomIdWithPageable(Long chatRoomId, LocalDateTime chattingTime, int limit);
 
-	// @Aggregation(pipeline = {
-	// 	"{ '$match': { 'chatRoomId' : ?0 } }",
-	// 	"{ '$sort' : { 'createdAt' : -1 } }",
-	// 	"{ '$limit' : ?1 }"
-	// })
-	// List<Chatting> findByChatRoomIdWithFirstPage(Long chatRoomId, int limit);
-
+	@Aggregation(pipeline = {
+		"{ '$match': { 'chatRoomId' : {'$in' : ?0}  ,  'readCount' : 1  } }",
+		"{ '$sort' : { 'createdAt' : -1 } }",
+		"{ '$group' : { '_id' : '$chatRoomId' , "
+			+ "'latestChatContent' : { '$last' : '$content'}, "
+			+ "'chatRoomId' : { '$last' : '$chatRoomId'}, "
+			+ "'createdAt' : { '$last' : '$createdAt'}, "
+			+ "'unreadChatCount' : { '$sum' : 1}}} "
+	})
+	List<ChatroomInfo> getChatDetails(List<Long> chatroomIds);
 }
