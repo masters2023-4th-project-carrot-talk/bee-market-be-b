44043db6713a69238b1ca0791be4d784b5c7a1bb
JJONSOO <pjs970616@naver.com>
2023-09-22 11:33:48 +0900

    feat: 메인 이미지 가져오기

--- be/market/src/main/java/com/carrot/market/product/infrastructure/ProductImageRepository.java
+++ be/market/src/main/java/com/carrot/market/product/infrastructure/ProductImageRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.carrot.market.image.domain.Image;
-import com.carrot.market.product.domain.Product;
 import com.carrot.market.product.domain.ProductImage;
 
 @Repository
@@ -17,6 +16,6 @@ public interface ProductImageRepository extends JpaRepository<ProductImage, Long
 	@Query("select pi.image from ProductImage pi join pi.image where pi.product.id = :productId")
 	List<Image> findImagesByProductId(@Param("productId") Long productId);
 
-	@Query("select pi from ProductImage pi join fetch Image i on pi.image = i where pi.product = :product")
-	List<ProductImage> findImageIdsByProduct(Product product);
+	@Query("select i from ProductImage pi join fetch Image i on pi.image = i where pi.product.id = :productId and pi.isMain = true ")
+	Image findMainImageIdByProduct(@Param("productId") Long productId);
 }
