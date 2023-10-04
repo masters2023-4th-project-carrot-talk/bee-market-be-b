0ab6b6d88623971e5350bb8f846906f0ef4a5ccf
JJONSOO <pjs970616@naver.com>
2023-10-04 11:01:10 +0900

    feat: jsonProperty isRead 필드에 추가

--- be/market/src/main/java/com/carrot/market/chat/presentation/dto/Message.java
+++ be/market/src/main/java/com/carrot/market/chat/presentation/dto/Message.java
@@ -3,6 +3,8 @@ package com.carrot.market.chat.presentation.dto;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,11 +18,16 @@ public class Message implements Serializable {
 	private Long chatroomId;
 	private String content;
 	private Long senderId;
+	@JsonProperty("isRead")
 	private boolean isRead;
 	private LocalDateTime chatTime;
 
 	public void readMessage() {
 		this.isRead = true;
+	}
+
+	public boolean getIsRead() { // 이거
+		return isRead;
 	}
 
 	public void setChatTime() {
@@ -33,6 +40,7 @@ public class Message implements Serializable {
 			" content='" + content + '\'' +
 			" chatroomId='" + chatroomId + '\'' +
 			" senderId='" + senderId + '\'' +
+			" isRead='" + isRead + '\'' +
 			'}';
 	}
 }
