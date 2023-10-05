ce0f4f7554301041d1c3fcb88ff0ae453a403f1b
code-tree <dltjdqls1200@naver.com>
2023-10-05 11:35:44 +0900

    fix: 컨트롤러에서 sseEmitter 반환

--- be/market/src/main/java/com/carrot/market/notification/presentation/NotificationController.java
+++ be/market/src/main/java/com/carrot/market/notification/presentation/NotificationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import com.carrot.market.member.presentation.annotation.Login;
 import com.carrot.market.member.resolver.MemberId;
@@ -22,13 +23,13 @@ public class NotificationController {
 	private final NotificationService notificationService;
 
 	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-	public ResponseEntity<Void> subscribe(
+	public ResponseEntity<SseEmitter> subscribe(
 		@Login MemberId memberId
 	) {
-		notificationService.subscribe(memberId.getMemberID());
-
+		final SseEmitter sseEmitter = notificationService.subscribe(memberId.getMemberID());
+		
 		return ResponseEntity.ok()
 			.header(NGINX_BUFFERING_OPTION_HEADER, DISABLED)
-			.build();
+			.body(sseEmitter);
 	}
 }
